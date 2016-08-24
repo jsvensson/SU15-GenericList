7926db7f5988a163408fd906ca54a4c3f77d502a
Johan Svensson <johan@atomicplayboy.net>
2016-08-24 14:28:40 +0200

    More efficient array growth
    Double size each time array fills

--- src/GenericList/GenericList.cs
+++ src/GenericList/GenericList.cs
@@ -7,14 +7,10 @@ namespace GenericList
 {
     public class GenericList<T>
     {
-        private T[] items;
+        private T[] items = new T[0];
+        private const int InitialSize = 1;
 
         public int Count { get; private set; }
-
-        public GenericList()
-        {
-            items = new T[0];
-        }
 
         public T this[int index]
         {
@@ -25,15 +21,23 @@ namespace GenericList
         public void Add(T value)
         {
             CheckCapacity(Count + 1);
-            items[items.Length - 1] = value;
+            items[Count] = value;
             Count++;
         }
 
         private void CheckCapacity(int size)
         {
+            // Initialize array if empty
+            if (items.Length == 0)
+            {
+                items = new T[InitialSize];
+                return;
+            }
+
+            // Already items in array?
             if (items.Length <= size - 1)
             {
-                T[] newItems = new T[Count + 1];
+                T[] newItems = new T[Count * 2];
                 Array.Copy(items, newItems, items.Length);
                 items = newItems;
             }
