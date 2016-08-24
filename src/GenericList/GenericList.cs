5fde3dff68bb37642fbc4f7d3f9b6685feac597e
Johan Svensson <johan@atomicplayboy.net>
2016-08-24 13:59:27 +0200

    Add project files.

---
+++ src/GenericList/GenericList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class GenericList<T>
+    {
+        private T[] items;
+
+        public int Count { get; private set; }
+
+        public GenericList()
+        {
+            items = new T[0];
+        }
+
+        public T this[int index]
+        {
+            get { return items[index]; }
+            set { items[index] = value; }
+        }
+
+        public void Add(T value)
+        {
+            CheckCapacity(Count + 1);
+            items[items.Length - 1] = value;
+            Count++;
+        }
+
+        private void CheckCapacity(int size)
+        {
+            if (items.Length <= size - 1)
+            {
+                T[] newItems = new T[Count + 1];
+                Array.Copy(items, newItems, items.Length);
+                items = newItems;
+            }
+        }
+
+    }
+}
---
+++ src/GenericList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numberList = new GenericList<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                numberList.Add(i);
+            }
+
+            Console.WriteLine($"Number of items in list: {numberList.Count}");
+
+            for (int i = 0; i < numberList.Count; i++)
+            {
+                Console.WriteLine(numberList[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
