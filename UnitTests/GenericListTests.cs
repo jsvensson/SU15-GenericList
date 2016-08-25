43a0269d918774c633fba6fd954b98861bb1e18b
Johan Svensson <johan@atomicplayboy.net>
2016-08-25 18:23:02 +0200

    Add some tests

---
+++ UnitTests/GenericListTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using GenericList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GenericListTests
+    {
+        [TestMethod]
+        public void GenericList__Add_Single_Item_To_Empty_List()
+        {
+            var actual = new GenericList<int>();
+            int expected = 1;
+            actual.Add(1);
+
+            Assert.AreEqual(actual[0], expected);
+            Assert.IsTrue(actual.Count == 1);
+        }
+
+        [TestMethod]
+        public void GenericList__Collection_Initializer()
+        {
+            var input = new GenericList<int> {1, 2, 3};
+            int[] expected = {1, 2, 3};
+
+            int[] actual = input.ToArray();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
